08ca9b6eb0f38670b2095e26048dd62ada3325a3
squeakyrobot <ryan@squeakyrobot.com>
2023-08-16 08:49:34 -0700

    Added better control of the html head

---
+++ src/lib/components/HeadMeta.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	export let url = $page.url.origin;
+	export let title = 'Best Conversation Ever';
+	export let description =
+		'A site where you can have captivating conversations with various AI personalities. But beware! We save all the messages so we can show them to your friends, family, colleagues, and neighbors.';
+</script>
+
+<title>Best Conversation Ever</title>
+<meta name="description" content={description} />
+<meta property="og:url" content={url} />
+<meta property="og:type" content="website" />
+<meta property="og:title" content={title} />
+<meta property="og:description" content={description} />
+<meta property="og:image" content="{$page.url.origin}/images/bce-logo-og.png" />
+<meta name="twitter:card" content="summary_large_image" />
+<meta property="twitter:domain" content={$page.url.hostname} />
+<meta property="twitter:url" content={url} />
+<meta name="twitter:title" content={title} />
+<meta name="twitter:description" content={description} />
+<meta name="twitter:image" content="{$page.url.origin}/images/bce-logo-og.png" />
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,15 @@
 <script lang="ts">
+	import { PUBLIC_RECAPTCHA_SITE_KEY } from '$env/static/public';
+	import HeadMeta from '$lib/components/HeadMeta.svelte';
 	import '../app.css';
 </script>
+
+<svelte:head>
+	<script
+		src="https://www.google.com/recaptcha/api.js?render={PUBLIC_RECAPTCHA_SITE_KEY}&trustedtypes=true"
+	></script>
+	<HeadMeta />
+</svelte:head>
 
 <div class="grid place-items-center h-[calc(100dvh)]">
 	<slot />
