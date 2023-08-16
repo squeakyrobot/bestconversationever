47a04983198e24c9867dd03faae6446590850acb
squeakyrobot <ryan@squeakyrobot.com>
2023-08-16 10:46:26 -0700

    Added hook to send noindex header on vercel preview sites

---
+++ src/hooks.server.ts
@@ -0,0 +1,11 @@
+
+export async function handle({ event, resolve }) {
+    const response = await resolve(event);
+
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview') {
+        response.headers.set('X-Robots-Tag', 'noindex');
+    }
+
+    return response;
+
+}
