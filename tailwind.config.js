ac8625b11ba8a16d3c53bc1bcb3459669222b9d6
Ryan <79234248+squeakyrobot@users.noreply.github.com>
2023-08-29 16:58:57 -0700

    Inbox Menu Indicator (#16)
    * Added indicator to inbox that touching the name will do something

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
-
+import * as defaultTheme from 'tailwindcss/defaultTheme';
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {},
-
+    screens: {
+      'xs': '380px',
+      ...defaultTheme.screens,
+    },
   },
   plugins: [
     require('daisyui'),
